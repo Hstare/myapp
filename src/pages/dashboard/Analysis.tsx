11ef4dbdc9acd6cc046c691b5b4aad4f7092db14
hexiaowei <1278313308@qq.com>
2019-11-18 00:06:32 +0800

    第三行数据修改

--- src/pages/dashboard/Analysis.tsx
+++ src/pages/dashboard/Analysis.tsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import { AnyAction, Dispatch } from 'redux';
-import { Card, Col, Divider, Icon, Row, Tooltip as AntdTooltip, Progress, List, DatePicker } from 'antd';
+import {
+  Card,
+  Col,
+  Divider,
+  Icon,
+  Row,
+  Tooltip as AntdTooltip,
+  Progress,
+  List,
+  DatePicker,
+  Dropdown,
+  Menu
+} from 'antd';
 import { connect } from 'dva';
 import {
   IAnalysisPayNumsType, IAnalysisPercentType, IAnalysisSalesType,
@@ -11,6 +23,7 @@ import { ConnectState } from '@/models/connect';
 import { Chart, Tooltip, Geom, Axis } from 'bizcharts';
 import styles from './analysis.less';
 import moment from 'moment';
+import { RangePickerValue } from 'antd/lib/date-picker/interface';
 
 interface IAnalysisProps {
   dispatch: Dispatch<AnyAction>;
@@ -30,7 +43,9 @@ interface IAnalysisInitState {
   key?: string,
   chartSaleAndVisitTitle?: string,
   listSaleAndVisitTitle?: string,
-  rangeDate?: [moment.Moment, moment.Moment],
+  // rangeDate?: [moment.Moment, moment.Moment],
+  rangeDate?: RangePickerValue,
+  selectedDate?: string,
 }
 
 const { RangePicker } = DatePicker;
@@ -50,6 +65,7 @@ class Analysis extends Component<IAnalysisProps, IAnalysisInitState> {
       chartSaleAndVisitTitle: '销售趋势',
       listSaleAndVisitTitle: '门店销售额排名',
       rangeDate: [moment(), moment()],
+      selectedDate: 'day',
     };
   }
 
@@ -97,9 +113,33 @@ class Analysis extends Component<IAnalysisProps, IAnalysisInitState> {
     }
   };
 
+  selectDate = (selectedDate: string) => {
+    let currentDateVar: RangePickerValue;
+    if (selectedDate === 'day') {
+      currentDateVar = [moment(), moment()];
+    } else if (selectedDate === 'week') {
+      currentDateVar = [moment().weekday(0), moment().weekday(6)];
+    } else if (selectedDate === 'month') {
+      const monthStart = `${moment().year()}+${moment().month() + 1}+'01'`;
+      const monthEnd = `${moment().year()}+${moment().month() + 1}+${moment().daysInMonth()}`;
+      currentDateVar = [moment(monthStart, 'YYYY-MM-DD'), moment(monthEnd, 'YYYY-MM-DD')];
+    } else if (selectedDate === 'year') {
+      const yearStart = `${moment().year()}+'01'+'01'`;
+      const yearEnd = `${moment().year()}+'12'+'31'`;
+      console.log(yearStart, yearEnd);
+      currentDateVar = [moment(yearStart, 'YYYY-MM-DD'), moment(yearEnd, 'YYYY-MM-DD')];
+    } else {
+      currentDateVar = [moment(), moment()];
+    }
+    // @ts-ignore
+    this.setState({ selectedDate, rangeDate: currentDateVar });
+    this.setState({ selectedDate });
+  };
+
   render() {
     const { loading, visits, payNumbers, percent, sales } = this.props;
-    const { key, chartSaleAndVisitTitle, listSaleAndVisitTitle, rangeDate } = this.state;
+    // eslint-disable-next-line max-len
+    const { key, chartSaleAndVisitTitle, listSaleAndVisitTitle, rangeDate, selectedDate } = this.state;
     console.log('loading', loading);
     console.log('percent', percent);
     console.log('sales', sales);
@@ -162,18 +202,40 @@ class Analysis extends Component<IAnalysisProps, IAnalysisInitState> {
       tab1: (<Row>{chartSaleAndVisit}</Row>),
       tab2: (<Row>{chartSaleAndVisit}</Row>),
     };
-    const salesExtra = (
-      <div>这是一段话
-        <RangePicker
-          defaultValue={rangeDate}
-          format="YYYY-MM-DD"
-          value={rangeDate}
+    const salesExtra = (<div>
+        <a onClick={() => this.selectDate('day')}
+           className={selectedDate === 'day' ? styles.currentDate : ''}> {'今日'}</a>
+        <a onClick={() => this.selectDate('week')}
+           className={selectedDate === 'week' ? styles.currentDate : ''}> {'本周'}</a>
+        <a onClick={() => this.selectDate('month')}
+           className={selectedDate === 'month' ? styles.currentDate : ''}> {'本月'}</a>
+        <a onClick={() => this.selectDate('year')}
+           className={selectedDate === 'year' ? styles.currentDate : ''}> {'全年'}</a>
+        <RangePicker style={{ width: 256 }}
+                     defaultValue={rangeDate}
+                     format="YYYY-MM-DD"
+                     value={rangeDate}
         />
-      </div>
+      </div>);
+    const onlineSearchExtraItem = (<Menu>
+        <Menu.Item>
+          操作一
+        </Menu.Item>
+        <Menu.Item>
+          操作二
+        </Menu.Item>
+        <Menu.Item>
+          操作三
+        </Menu.Item>
+      </Menu>
     );
+    const onlineSearchExtra = (
+      <Dropdown overlay={onlineSearchExtraItem}>
+        <Icon type="ellipsis" />
+      </Dropdown>
+    )
     return (
       <div>
-        这是一个分析页
         <Row gutter={16} style={{ paddingTop: 20 }}>
           <Col span={6}>
             <Card>
@@ -273,6 +335,34 @@ class Analysis extends Component<IAnalysisProps, IAnalysisInitState> {
             <Card tabBarExtraContent={salesExtra} tabList={tabList} activeTabKey={key}
                   onTabChange={tabKey => this.onTabChange(tabKey)}>
               {contentList[key]}
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={16} style={{ paddingTop: 20 }}>
+          <Col span={12}>
+            <Card title="线上热门搜索" extra={onlineSearchExtra}>
+              <Row>
+                <Col span={12} style={{ paddingLeft: 34, paddingRight: 34, marginBottom: 24 }}>
+                  <Row>
+                    <Col span={16}><span style={{ color: 'rgba(0,0,0,.45)' }}>搜索用户数</span></Col>
+                    <Col span={6}><AntdTooltip title="指标说明"><Icon type="info-circle"/></AntdTooltip></Col>
+                  </Row>
+                  <Row style={{ height: 32, marginTop: 8 }} type="flex" align="top">
+                    <Col span={12}><span style={{ fontSize: 24 }}>12,321</span></Col>
+                    <Col span={12}><span style={{ fontSize: 16 }}>17.1</span><Icon type="caret-up" style={{ color: 'red' }}/></Col>
+                  </Row>
+                </Col>
+                <Col span={12} style={{ paddingLeft: 34, paddingRight: 34, marginBottom: 24 }}>
+                  <Row>
+                    <Col span={16}><span style={{ color: 'rgba(0,0,0,.45)' }}>人均搜索次数</span></Col>
+                    <Col span={6}><AntdTooltip title="指标说明"><Icon type="info-circle"/></AntdTooltip></Col>
+                  </Row>
+                  <Row style={{ height: 32, marginTop: 8 }} type="flex" align="top">
+                    <Col span={12}><span style={{ fontSize: 24 }}>2.7</span></Col>
+                    <Col span={12}><span style={{ fontSize: 16 }}>26.2</span><Icon type="caret-down" style={{ color: 'green' }}/></Col>
+                  </Row>
+                </Col>
+              </Row>
             </Card>
           </Col>
         </Row>
